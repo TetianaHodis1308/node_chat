a862ff75d0005ff444cb6a31ae67fb3e1a56a396
TetianaHodis1308 <hodistetianajob@ukr.net>
2026-06-03 16:43:43 +0300

    main solution

---
+++ src/client/protocol.js
@@ -0,0 +1,18 @@
+'use strict';
+
+window.ChatConstants = {
+  MessageType: {
+    SET_USERNAME: 'setUsername',
+    USERNAME_SET: 'usernameSet',
+    SEND_MESSAGE: 'sendMessage',
+    MESSAGE: 'message',
+    CREATE_ROOM: 'createRoom',
+    RENAME_ROOM: 'renameRoom',
+    JOIN_ROOM: 'joinRoom',
+    DELETE_ROOM: 'deleteRoom',
+    ROOMS_UPDATED: 'roomsUpdated',
+    ROOM_HISTORY: 'roomHistory',
+    ERROR: 'error',
+  },
+  USERNAME_STORAGE_KEY: 'username',
+};
